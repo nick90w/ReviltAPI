8c0f3e94d8e9f4724780e5549dfc368568b71cb5
peter <72741411+PeterB2003@users.noreply.github.com>
2021-06-23 13:09:11 +0200

    Images en contact info toegevoegd
    - image voor eerste textarea
    
    - contact info

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -40,35 +40,3 @@ anime.timeline({ loop: true })
 //=========================================
 
 
-
-
-/*
-
-
-function revealMessage() {
-    document.getElementById("Hidden").style.display = "block";
-    window.addEventListener("scroll", reveal);
-
-    function reveal() {
-        var reveals = document.querySelectorAll('.Hidden')
-
-        for (var i = 0; i < reveals.length; i++)
-
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 0;
-
-        if (revealtop < windowheight - revealpoint)
-        {
-            reveals[i].classList.add('active');
-
-        }
-        else
-        {
-            reveals[i].classList.remove('active');
-
-        }
-    }
-
-}
-*/
